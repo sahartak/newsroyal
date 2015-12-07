ad59ac96b56d9b189dc64b8da7f3bc3e422643a9
grigory-web <grigori.podosyan@gmail.com>
2015-12-07 17:12:03 +0400

    index

--- fb-api/index.php
+++ fb-api/index.php
@@ -1,5 +1,5 @@
 <?php
-die('z59');
+echo 'ok <br>';
 session_start();
 define('FACEBOOK_SDK_V4_SRC_DIR', 'facebook-sdk-v5/');
 require_once 'facebook-sdk-v5/autoload.php';
@@ -9,9 +9,11 @@ $fb = new Facebook\Facebook(array(
   'app_secret' => '5e3c1a44bf709e2be25a3b3f4bb2ec9d',
   'default_graph_version' => 'v2.5',
   ));
-  
+  echo 'ok <br>';
 $helper = $fb->getRedirectLoginHelper();
+echo 'ok <br>';
 $permissions = array('email', 'user_likes'); // optional
+echo 'ok <br>';
 $loginUrl = $helper->getLoginUrl('http://newsroyal.com/fb-api/login.php', $permissions);
-
+echo 'ok <br>';
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
